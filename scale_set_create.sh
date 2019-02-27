5daeb0c9547cec49f83931debf7e0fc1496ea2dd
Stas Pavlov <stanisp@microsoft.com>
2019-02-27 20:16:15 +0300

    fix SAS token gen issue

--- scale_set_create.sh
+++ scale_set_create.sh
@@ -17,6 +17,9 @@ export AZURE_SCALESET_VM_SKU=Standard_B1s
 export AZURE_SA_NAME=metricsstorageaccount	
 
 
+# Login to start script
+az login
+
 # Set required subscription
 az account set -s $AZURE_SUBSCRIPTION_ID
 
@@ -36,27 +39,25 @@ az vmss create -n $AZURE_SCALESET_NAME -g $AZURE_RG_NAME \
 az storage account create -n $AZURE_SA_NAME -l $AZURE_DC_LOCATION -g $AZURE_RG_NAME --sku Standard_LRS
 
 # Create SAS token expiry date +1 year to current datetime
-export AZURE_SA_SAS_EXPIRY_DATE=`date -d "1 year" '+%Y-%m-%dT%H:%MZ'`
+export AZURE_SA_SAS_EXPIRY_DATE=`date -d "1 year" '+%Y-%m-%dT%H:%M:00Z'`
+export AZURE_SA_SAS_START_DATE=`date -d "-1 year" '+%Y-%m-%dT%H:%M:00Z'`
 # Get Azure SA Key
-export AZURE_SA_KEY=`az storage account keys list -n $AZURE_SA_NAME -g $AZURE_RG_NAME --query [0].value --output tsv`
+# export AZURE_SA_KEY=`az storage account keys list -n $AZURE_SA_NAME -g $AZURE_RG_NAME --query [0].value --output tsv`
 # Get SAS token
-export AZURE_SA_SAS_TOKEN=`az storage account generate-sas --permissions cdlruwap --account-name $AZURE_SA_NAME --account-key $AZURE_SA_KEY --services bfqt --resource-types sco --expiry $AZURE_SA_SAS_EXPIRY_DATE --output tsv`
+export AZURE_SA_SAS_TOKEN=`az storage account generate-sas --permissions acluw --account-name $AZURE_SA_NAME --services bt --resource-types co --expiry $AZURE_SA_SAS_EXPIRY_DATE --start $AZURE_SA_SAS_START_DATE --output tsv`
 
 export AZURE_SCALESET_ID=`az vmss list --resource-group $AZURE_RG_NAME  --query [0].id --output tsv`
 
 
-# Replace placeholders with actual values in secret files
-sed -i "s/StorageName/$AZURE_SA_NAME/g" storage_secret.json
-#sed -i 's@StorageSASToken@'$AZURE_SA_SAS_TOKEN'@' storage_secret.json
-sed -i "s/StorageSASToken/$(echo $AZURE_SA_SAS_TOKEN | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" storage_secret.json
+STORAGE_SECRET="{'storageAccountName': '$AZURE_SA_NAME', 'storageAccountSasToken': '$AZURE_SA_SAS_TOKEN'}"
 
-sed -i "s/TRStorageName/$AZURE_SA_NAME/g" metrics_config.json
-sed -i 's@TRResourceID@'$AZURE_SCALESET_ID'@' metrics_config.json
+az vmss diagnostics get-default-config > default_config.json
 
+sed -i "s#__DIAGNOSTIC_STORAGE_ACCOUNT__#$AZURE_SA_NAME#g" default_config.json
+sed -i "s#__VM_OR_VMSS_RESOURCE_ID__#$AZURE_SCALESET_ID#g" default_config.json
 
 # Add metrics as sepcified in metrics_config.json to scale set
 az vmss diagnostics set --resource-group $AZURE_RG_NAME \
                         --vmss-name $AZURE_SCALESET_NAME \
-                        --settings  metrics_config.json \
-                        --protected-settings storage_secret.json
-
+                        --settings  default_config.json \
+                        --protected-settings "${STORAGE_SECRET}"
