576031942c826c9262cbea95903ab6d224fd7fb4
Victor Kiselev <gaploid@gmail.com>
2019-03-18 14:02:22 +0300

    Added min num of nodes param

--- httpTriggerAutoScale/httpTriggerAutoScale/WadMetric.cs
+++ httpTriggerAutoScale/httpTriggerAutoScale/WadMetric.cs
@@ -22,6 +22,7 @@ namespace httpTriggerAutoScale
         public string CounterName { get; set; }
         public string DeploymentId { get; set; }
         public string Host { get; set; }
+        public string RoleInstance { get; set; }
         public double Last { get; set; }
         public double Maximum { get; set; }
         public double Minimum { get; set; }
